a9bfc51926fa4ba5419d53617276c5bb8eb97c3d
Arjun Anoop <68767302+arjunanoop101@users.noreply.github.com>
2025-08-11 12:35:31 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://fooddelivery-backend-xwfr.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
@@ -91,5 +91,6 @@ const StoreContextProvider = (props) => {
     )
 
 }
+
 
 export default StoreContextProvider;
